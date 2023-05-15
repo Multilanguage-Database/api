88f822fc0f1e6e48b3d231766e59755229b3dae9
ep-aotto <aotto@epages.com>
2023-05-15 06:47:03 +0200

    inital commit

---
+++ src/main/java/com/multiLanguageDB/multilanguageapi/MultiLanguageApiApplication.java
@@ -0,0 +1,13 @@
+package com.multiLanguageDB.multilanguageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiLanguageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiLanguageApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multiLanguageDB/multilanguageapi/MultiLanguageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiLanguageDB.multilanguageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiLanguageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
